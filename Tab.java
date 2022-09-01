3d75792e3e6525fb8d02d64c5d7188e31ea26d55
Piyushv56 <102844917+Piyushv56@users.noreply.github.com>
2022-09-01 16:09:51 +0530

    Update Tab.java

--- Tab.java
+++ Tab.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.util.Scanner; // scanner package imported
 
 public class Tab {
 
 	public static void main(String[] args) {
-		try (// TODO Auto-generated method stub
+		//Taking inuput from user
 Scanner sc = new Scanner (System.in)) {
-	System.out.println("Enter the number = ");
-	int num = sc.nextInt();
+	System.out.println("Enter the number = "); // number which table is to be print
+	int num = sc.nextInt();               // loop started and execute till i=10
 	for (int i =1;i<=10;i++) {
-		System.out.println(num+"*"+i+"="+num*i);
+		System.out.println(num+"*"+i+"="+num*i);  // printing the table of number which is enterd by user
 	}
 }
 	}
