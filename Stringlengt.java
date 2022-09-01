47cdf1e3314f1139c756781692dabee636ff179c
Piyushv56 <102844917+Piyushv56@users.noreply.github.com>
2022-09-01 16:12:13 +0530

    Update Stringlengt.java

--- Stringlengt.java
+++ Stringlengt.java
@@ -1,11 +1,14 @@
+//Write a program to count number of char in a string entered by user excluding space//
 import java.util.Scanner;
 
 public class Stringlengt {
 public static void main (String[]piyush) {
+	// Taking Input from the user
 	Scanner sc = new Scanner(System.in);
 	int count =0,i=0;
 	System.out.println("Enter The Character to count");
-String str = sc.nextLine();
+String str = sc.nextLine(); 
+	//using do while loop  to calculate the string in character
 do {
 	if (str.charAt(i) != ' ')
 	{
